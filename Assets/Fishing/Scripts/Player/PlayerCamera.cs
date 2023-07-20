224bc59a247ea47584f7ed3b1214939ff5f2525b
CA01972058 <CA01972058@st.kawahara.ac.jp>
2023-07-20 12:09:55 +0900

    プレイヤーの動き

---
+++ Assets/Fishing/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField, Header("感度")]
+    float sensitivity = 3.0f;
+    [SerializeField, Header("プレイヤー")]
+    PlayerMove PlayerMove;
+
+
+    GameObject  m_parentObject;     //親オブジェクト。
+    Quaternion  m_cameraRot;        //カメラの回転。
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //親オブジェクトを取得。
+        m_parentObject = transform.parent.gameObject;
+
+        //カメラの初期回転を取得。
+        m_cameraRot = m_parentObject.transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!PlayerMove.GetCursorLock())
+        {
+            return;
+        }
+
+        //注視点をプレイヤーに合わせる。
+        m_parentObject.transform.position = PlayerMove.transform.position;
+
+        //マウスの入力。
+        float xRot = Input.GetAxis("Mouse X") * sensitivity;
+
+        //オイラー角を使用して角度を設定。
+        m_cameraRot *= Quaternion.Euler(0, xRot, 0);
+
+        m_parentObject.transform.localRotation = m_cameraRot;
+    }
+}
--- Assets/Fishing/Scripts/Player/PlayerMove.cs
+++ Assets/Fishing/Scripts/Player/PlayerMove.cs
@@ -4,15 +4,183 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, Header("移動速度")]
+    float Speed = 1.0f;
+    [SerializeField, Header("一度にする回転量")]
+    float RotateStep = 1.0f;
+    [SerializeField, Header("カメラ")]
+    GameObject GameCamera;
+
+
+    /// <summary>
+    /// プレイヤーステート。
+    /// </summary>
+    enum PlayerState
     {
-        
+        enState_Idle,
+        enState_Walk,
     }
 
-    // Update is called once per frame
-    void Update()
+    Rigidbody       m_rigidbody;            //Rigidbody。
+    Animator        m_animator;             //Animator。
+
+    Vector3         m_moveSpeed;            //移動速度。
+    PlayerState     m_playerState;          //プレイヤーステート。
+    bool            m_cursorLock = false;   //カーソルのロック状態。
+
+
+    /// <summary>
+    /// カーソルのロック状態を取得。
+    /// </summary>
+    /// <returns></returns>
+    public bool GetCursorLock()
     {
-        
+        return m_cursorLock;
+    }
+
+
+    private void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+        m_animator = GetComponent<Animator>();
+
+        //カーソルをロック状態にする。
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!GetCursorLock())
+        {
+            return;
+        }
+
+        Move();
+
+        Rotation();
+
+        State();
+    }
+
+    private void Update()
+    {
+        UpdateCursorLock();
+    }
+
+    /// <summary>
+    /// 移動処理。
+    /// </summary>
+    void Move()
+    {
+        //移動速度と方向を初期化。
+        m_moveSpeed = Vector3.zero;
+        Vector3 moveDirection = Vector3.zero;
+
+        //移動方向を入力。
+        moveDirection.x = Input.GetAxisRaw("Horizontal");
+        moveDirection.z = Input.GetAxisRaw("Vertical");
+
+        //カメラを考慮した移動。
+        Vector3 forward = GameCamera.transform.forward;
+        Vector3 right = GameCamera.transform.right;
+        forward.y = 0.0f;
+        right.y = 0.0f;
+        forward *= moveDirection.z;
+        right *= moveDirection.x;
+
+        //移動速度に上記で計算したベクトルを合わせる。
+        m_moveSpeed += forward + right;
+        //正規化する。
+        m_moveSpeed.Normalize();
+
+        //移動方向と移動速度を合わせる。
+        m_moveSpeed *= Speed;
+
+        //移動方向に力を加える。
+        m_rigidbody.AddForce(m_moveSpeed, ForceMode.Acceleration);
+    }
+
+    /// <summary>
+    /// 回転処理。
+    /// </summary>
+    void Rotation()
+    {
+        //入力があったなら。
+        if (m_moveSpeed.sqrMagnitude > 0.0f)
+        {
+            //プレイヤーの方向へ回転する。
+            Quaternion rot = Quaternion.LookRotation(m_moveSpeed);
+
+            //回転を徐々に与える。
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, RotateStep);
+        }
+    }
+
+    /// <summary>
+    /// ステート処理。
+    /// </summary>
+    void State()
+    {
+        switch (m_playerState)
+        {
+        case PlayerState.enState_Idle:
+                ProcessCommonState();
+            break;
+
+        case PlayerState.enState_Walk:
+                ProcessCommonState();
+            break;
+        }
+
+    }
+
+    /// <summary>
+    /// ステートの共通処理。
+    /// </summary>
+    void ProcessCommonState()
+    {
+        if(m_playerState == PlayerState.enState_Idle)
+        {
+            if (m_moveSpeed.sqrMagnitude > 0.0f)
+            {
+                //移動状態にする。
+                m_animator.SetBool("MoveFlag", true);
+                m_playerState = PlayerState.enState_Walk;
+            }
+        }
+        else if(m_playerState == PlayerState.enState_Walk)
+        {
+            if (m_moveSpeed.sqrMagnitude == 0.0f)
+            {
+                //待機状態にする。
+                m_animator.SetBool("MoveFlag", false);
+                m_playerState = PlayerState.enState_Idle;
+            }
+        }
+    }
+
+    /// <summary>
+    /// カーソルの固定処理。
+    /// </summary>
+    void UpdateCursorLock()
+    {
+        if (m_cursorLock)
+        {
+            //Escが押されたらロック解除。
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                m_cursorLock = false;
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+        else
+        {
+            //マウスの入力でロック。
+            if (Input.GetMouseButton(0))
+            {
+                m_cursorLock = true;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
     }
 }
